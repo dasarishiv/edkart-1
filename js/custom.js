c7321ea34b23034ff383a13da1474711118796bd
itsmeui <softdazzle@gmail.com>
2019-11-18 23:16:05 +0530

    chages

--- js/custom.js
+++ js/custom.js
@@ -92,7 +92,34 @@ $(document).ready(function(){
     });
 
 
+    $('.blog-list').owlCarousel({
+        loop:true,
+        margin:40,
+        responsiveClass:true,
+ 
+        responsive:{
+            0:{
+                items:1,
+                nav:false,
+                dots:true,
+                autoplay:true,
+                autoplayTimeout:3000
 
+
+            },
+            600:{
+                items:2,
+                nav:false,
+                dots:true
+            },
+            1000:{
+                items:3,
+                nav:false,
+                loop:false,
+                dots:true
+            }
+        }
+    });
 
     $('.like').click(function(){
 
